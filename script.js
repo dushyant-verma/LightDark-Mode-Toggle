b54be993e00408ecd72ff938ddc1ffae9afb6990
dushyant verma <sde.dushyant@gmail.com>
2024-06-28 17:42:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('toggleButton');
+const body = document.body;
+
+
+//  toggling on the button 
+
+toggleButton.addEventListener('click', () => {
+
+body.classList.toggle('dark-mode');   // changing the body color
+
+
+
+});
